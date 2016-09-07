d55bba3a6f93bbd5536854274abd388699402cc9
Fougeroc <charlesfougeron@gmail.com>
2016-09-07 12:57:08 +0200

    adding some experiments on dimension 2 case

---
+++ tomography_2.py
@@ -0,0 +1,24 @@
+from template import Experiment, TorusPlanarSection, lin_space
+
+from pylab import *
+from matplotlib import *
+
+#############################################################################################
+#                                    EXPERIMENT 1                                           #
+#############################################################################################
+
+# Dimension 2
+# Tomography
+
+n_step = 100
+d_min = 0.01
+d_max = 0.99
+
+def shift(s):
+    def section_fun(rel, dist):
+        return Experiment([rel, s + 2*rel], [0., dist], rel, dist)
+    return(section_fun)
+
+for s in np.arange(0,6,.1):
+    t = TorusPlanarSection(2, shift(s), '0,x_r,2r+' + str(s), d_min, d_max/2, d_min, d_max)
+    t.compute_discretized(100, 100, nb_iterations=10**5)
